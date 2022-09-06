91f11a6b7b00fa5909c9449bc58ecdb693820356
synder934 <tobyred8@hotmail.co.uk>
2022-09-06 22:30:06 +0100

    boilerplate

--- main.py
+++ main.py
@@ -1,48 +1,26 @@
 import pygame as pg
 
-
-DIM = (30*10, 30*10)
-TILESIZE = (30, 30)
-
+DIM = [1000, 700]
+vec3 = pg.Vector3
 
 
-class Game():
-    def __init__(self) -> None:
-        pg.init()
-        self.disp = pg.display.set_mode(DIM)
-        self.mainloop()
-        pass
-
-    
-    def setup(self):
-
-        pass
-
-
-    def mainloop(self):
-        while True:
-            self.disp.fill((100, 100, 100))
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    self.quit()
-
-
-            
-
-
-
-
-
-            pg.display.update()
-
-    
-
-
-
-    def quit(self):
-        pg.quit()
-        quit()
 
 
 if __name__ == '__main__':
-    Game()
+    disp = pg.display.set_mode(DIM)
+
+    running = 1
+    while running:
+        for e in pg.event.get():
+            if e.type == pg.QUIT:
+                running = 0
+
+        disp.fill((0,0,0))
+
+
+
+
+
+        pg.display.update()        
+
+    pass
